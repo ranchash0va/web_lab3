8b3fdecef033f88105f6c13ba23fc9f86e3b0f6d
ranchash0va <GITHUB ranchashova.di@gmail.com>
2022-05-09 00:37:12 +0200

    Добавьте файлы проекта.

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿ document.querySelector('.button').addEventListener('click', function () {
+    document.querySelector('.cont').classList.add('visibl');
+});
+
+ document.querySelector('.overlay').addEventListener('click', function () {
+    document.querySelector('.cont').classList.remove('visibl');
+});
+  document.querySelector('.button').addEventListener('click', function () {
+    document.querySelector('.cont').classList.add('visibl');
+});
+
+ document.querySelector('.overlay').addEventListener('click', function () {
+    document.querySelector('.cont').classList.remove('visibl');
+});
+ 
